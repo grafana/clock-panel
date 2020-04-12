b8d71733640cd52144d074040cac979956c5cbad
Ryan McKinley <ryantxu@gmail.com>
2020-04-12 14:13:22 -0700

    split out the options

---
+++ src/options.ts
@@ -0,0 +1,228 @@
+import { PanelOptionsEditorBuilder } from '@grafana/data';
+
+import { ClockOptions, ClockMode, ClockType, FontWeight, ZoneFormat } from './types';
+
+export const optionsBuilder = (builder: PanelOptionsEditorBuilder<ClockOptions>) => {
+  // Global options
+  builder
+    .addRadio({
+      path: 'mode',
+      name: 'Mode',
+      settings: {
+        options: [
+          { value: ClockMode.time, label: 'Time' },
+          { value: ClockMode.countdown, label: 'Countdown' },
+        ],
+      },
+      defaultValue: ClockMode.time,
+    })
+    .addColorPicker({
+      path: 'bgColor',
+      name: 'Background Color',
+    });
+
+  addCountdown(builder);
+  addTimeFormat(builder);
+  addTimeZone(builder);
+  addDateFormat(builder);
+};
+
+//---------------------------------------------------------------------
+// COUNTDOWN
+//---------------------------------------------------------------------
+function addCountdown(builder: PanelOptionsEditorBuilder<ClockOptions>) {
+  const category = ['Countdown'];
+
+  builder
+    .addTextInput({
+      category,
+      path: 'countdownSettings.endCountdownTime',
+      name: 'End Time',
+      settings: {
+        placeholder: 'todo....',
+      },
+      defaultValue: 'TODO',
+      showIf: o => o.mode === ClockMode.countdown,
+    })
+
+    .addTextInput({
+      category,
+      path: 'countdownSettings.endText',
+      name: 'End Text',
+      settings: {
+        placeholder: 'todo....',
+      },
+      defaultValue: '00:00:00',
+      showIf: o => o.mode === ClockMode.countdown,
+    })
+
+    .addTextInput({
+      category,
+      path: 'countdownSettings.customFormat',
+      name: 'Custom format',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: undefined,
+      showIf: o => o.mode === ClockMode.countdown,
+    });
+}
+
+//---------------------------------------------------------------------
+// TIME FORMAT
+//---------------------------------------------------------------------
+function addTimeFormat(builder: PanelOptionsEditorBuilder<ClockOptions>) {
+  const category = ['Time Format'];
+
+  builder
+    .addRadio({
+      category,
+      path: 'clockType',
+      name: 'Clock Type',
+      settings: {
+        options: [
+          { value: ClockType.H24, label: '24 Hour' },
+          { value: ClockType.H12, label: '12 Hour' },
+          { value: ClockType.Custom, label: 'Custom' },
+        ],
+      },
+      defaultValue: ClockType.H24,
+    })
+    .addTextInput({
+      category,
+      path: 'timeSettings.customFormat',
+      name: 'Time Format',
+      description: 'the date formatting pattern',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: undefined,
+      showIf: opts => opts.clockType === ClockType.Custom,
+    })
+    .addTextInput({
+      category,
+      path: 'timeSettings.fontSize',
+      name: 'Font size',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: '12px',
+    })
+    .addRadio({
+      category,
+      path: 'timeSettings.fontWeight',
+      name: 'Font weight',
+      settings: {
+        options: [
+          { value: FontWeight.normal, label: 'Normal' },
+          { value: FontWeight.bold, label: 'Bold' },
+        ],
+      },
+      defaultValue: FontWeight.normal,
+    });
+}
+
+//---------------------------------------------------------------------
+// TIMEZONE
+//---------------------------------------------------------------------
+function addTimeZone(builder: PanelOptionsEditorBuilder<ClockOptions>) {
+  const category = ['Timezone'];
+
+  builder
+    .addTextInput({
+      category,
+      path: 'timezone',
+      name: 'Timezone',
+      settings: {
+        placeholder: '(TODO... should be picker!)',
+      },
+      defaultValue: undefined,
+    })
+    .addBooleanSwitch({
+      category,
+      path: 'timezoneSettings.showTimezone',
+      name: 'Show Timezone',
+      defaultValue: false,
+    })
+    .addSelect({
+      category,
+      path: 'timezoneSettings.zoneFormat',
+      name: 'Display Format',
+      settings: {
+        options: [
+          { value: ZoneFormat.name, label: 'Normal' },
+          { value: ZoneFormat.nameOffset, label: 'Name + Offset' },
+          { value: ZoneFormat.offsetAbbv, label: 'Offset + Abbreviation' },
+          { value: ZoneFormat.offset, label: 'Offset' },
+          { value: ZoneFormat.abbv, label: 'Abbriviation' },
+        ],
+      },
+      defaultValue: ZoneFormat.offsetAbbv,
+    })
+    .addTextInput({
+      category,
+      path: 'timezoneSettings.fontSize',
+      name: 'Font size',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: '12px',
+    })
+    .addRadio({
+      category,
+      path: 'timezoneSettings.fontWeight',
+      name: 'Font weight',
+      settings: {
+        options: [
+          { value: FontWeight.normal, label: 'Normal' },
+          { value: FontWeight.bold, label: 'Bold' },
+        ],
+      },
+      defaultValue: FontWeight.normal,
+    });
+}
+
+//---------------------------------------------------------------------
+// DATE FORMAT
+//---------------------------------------------------------------------
+function addDateFormat(builder: PanelOptionsEditorBuilder<ClockOptions>) {
+  const category = ['Date Options'];
+
+  builder
+    .addBooleanSwitch({
+      category,
+      path: 'dateSettings.showDate',
+      name: 'Show Date',
+      defaultValue: false,
+    })
+    .addTextInput({
+      category,
+      path: 'dateSettings.dateFormat',
+      name: 'Date Format',
+      settings: {
+        placeholder: '(TODO... should be picker!)',
+      },
+      defaultValue: 'YYYY-MM-DD',
+    })
+    .addTextInput({
+      category,
+      path: 'dateSettings.fontSize',
+      name: 'Font size',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: '20px',
+    })
+    .addRadio({
+      category,
+      path: 'dateSettings.fontWeight',
+      name: 'Font weight',
+      settings: {
+        options: [
+          { value: FontWeight.normal, label: 'Normal' },
+          { value: FontWeight.bold, label: 'Bold' },
+        ],
+      },
+      defaultValue: FontWeight.normal,
+    });
+}
