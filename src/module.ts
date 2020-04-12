fd117c77805fedf05bbf6c040ba9050944cb19bd
Ryan McKinley <ryantxu@gmail.com>
2020-04-11 23:45:49 -0700

    use react

--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,122 @@
-import { ClockCtrl } from './clock_ctrl';
+import { PanelPlugin } from '@grafana/data';
 
-export { ClockCtrl as PanelCtrl };
+import { ClockPanel } from './ClockPanel';
+import { ClockOptions, ClockMode, ClockType, defaults, FontWeight, ZoneFormat } from './types';
+
+export const plugin = new PanelPlugin<ClockOptions>(ClockPanel).setDefaults(defaults).setPanelOptions(builder => {
+  builder
+    .addRadio({
+      path: 'mode',
+      name: 'Mode',
+      settings: {
+        options: [
+          { value: ClockMode.time, label: 'Time' },
+          { value: ClockMode.countdown, label: 'Countdown' },
+        ],
+      },
+      defaultValue: ClockMode.time,
+    })
+    .addColorPicker({
+      path: 'bgColor',
+      name: 'Background Color',
+    })
+    .addRadio({
+      category: ['Time Format'],
+      path: 'clockType',
+      name: 'Clock Type',
+      settings: {
+        options: [
+          { value: ClockType.H24, label: '24 Hour' },
+          { value: ClockType.H12, label: '12 Hour' },
+          { value: ClockType.Custom, label: 'Custom' },
+        ],
+      },
+      defaultValue: ClockType.H24,
+    })
+    .addTextInput({
+      category: ['Time Format'],
+      path: 'timeSettings.customFormat',
+      name: 'Time Format',
+      description: 'the date formatting pattern',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: undefined,
+      showIf: opts => opts.clockType === ClockType.Custom,
+    })
+    .addTextInput({
+      category: ['Time Format'],
+      path: 'timeSettings.fontSize',
+      name: 'Font size',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: '12px',
+    })
+    .addRadio({
+      category: ['Time Format'],
+      path: 'timeSettings.fontWeight',
+      name: 'Font weight',
+      settings: {
+        options: [
+          { value: FontWeight.normal, label: 'Normal' },
+          { value: FontWeight.bold, label: 'Bold' },
+        ],
+      },
+      defaultValue: FontWeight.normal,
+    })
+    .addTextInput({
+      category: ['Timezone'],
+      path: 'timezone',
+      name: 'Timezone',
+      settings: {
+        placeholder: '(TODO... should be picker!)',
+      },
+      defaultValue: undefined,
+    })
+    .addBooleanSwitch({
+      category: ['Timezone'],
+      path: 'timezoneSettings.showTimezone',
+      name: 'Show Timezone',
+      defaultValue: false,
+    })
+    .addSelect({
+      category: ['Timezone'],
+      path: 'timezoneSettings.zoneFormat',
+      name: 'Display Format',
+      settings: {
+        options: [
+          { value: ZoneFormat.name, label: 'Normal' },
+          { value: ZoneFormat.nameOffset, label: 'Name + Offset' },
+          { value: ZoneFormat.offsetAbbv, label: 'Offset + Abbreviation' },
+          { value: ZoneFormat.offset, label: 'Offset' },
+          { value: ZoneFormat.abbv, label: 'Abbriviation' },
+        ],
+      },
+      defaultValue: ZoneFormat.offsetAbbv,
+    })
+    .addTextInput({
+      category: ['Timezone'],
+      path: 'timezoneSettings.fontSize',
+      name: 'Font size',
+      settings: {
+        placeholder: 'date format',
+      },
+      defaultValue: '12px',
+    })
+    .addRadio({
+      category: ['Timezone'],
+      path: 'timezoneSettings.fontWeight',
+      name: 'Font weight',
+      settings: {
+        options: [
+          { value: FontWeight.normal, label: 'Normal' },
+          { value: FontWeight.bold, label: 'Bold' },
+        ],
+      },
+      defaultValue: FontWeight.normal,
+    });
+});
+
+// : string;
+// fontWeight: string;
