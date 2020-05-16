d1abeb986ccc67d9c2d13d666af01255bd269ac3
CircleCI Automation <eng@grafana.com>
2020-05-16 01:52:42 +0000

    automated release 1.1.0 [skip ci]

--- src/module.ts
+++
@@ -1,7 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-
-import { ClockPanel } from './ClockPanel';
-import { ClockOptions } from './types';
-import { optionsBuilder } from './options';
-
-export const plugin = new PanelPlugin<ClockOptions>(ClockPanel).setNoPadding().setPanelOptions(optionsBuilder);
