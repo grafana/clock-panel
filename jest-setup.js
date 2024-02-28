2c95ef37f1e7eb50d3193c8d9ec05b1fa214c48f
Esteban Beltran <academo@users.noreply.github.com>
2024-02-28 14:33:56 +0100

    Update create-plugin configuration. Bump to 2.1.4 (#177)
    * Update create-plugin configuration
    
    * v2.1.4
    
    * Use node 18

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,5 @@
 // Jest setup provided by Grafana scaffolding
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, { TextDecoder, TextEncoder });
 import './.config/jest-setup';
