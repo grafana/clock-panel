866f177f4e944fb80a6f3408a6cfc005cf6d3b2b
Esteban Beltran <academo@users.noreply.github.com>
2024-11-11 10:45:06 +0100

    Chore: Update to latest create-plugin (#220)
    * Update create plugin
    
    * Install react-router-dom
    
    * Add tests
    
    * Fix eslint
    
    * Fix e2e
    
    * Force grafana min version

--- jest-setup.js
+++ jest-setup.js
@@ -1,5 +1,2 @@
 // Jest setup provided by Grafana scaffolding
-import { TextEncoder, TextDecoder } from 'util';
-
-Object.assign(global, { TextDecoder, TextEncoder });
 import './.config/jest-setup';
