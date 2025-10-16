955e90e65832d1bcb0f628b1cca37e21de755dee
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-10-16 08:17:19 +0200

    chore: only update en-US

--- i18next.config.ts
+++ i18next.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'i18next-cli';
 import pluginJson from './src/plugin.json';
 
 export default defineConfig({
-  locales: pluginJson.languages,
+  locales: ['en-US'], // Only en-US  is updated - Crowdin will PR with other languages
   extract: {
     input: ['src/**/*.{tsx,ts}'],
     output: 'src/locales/{{language}}/{{namespace}}.json',
