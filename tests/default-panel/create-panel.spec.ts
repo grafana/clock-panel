d7266367f6e0893e8bc67d2cf1251e648be199e4
Levente Balogh <balogh.levente.hu@gmail.com>
2025-11-05 09:53:56 +0100

    Test

--- tests/default-panel/create-panel.spec.ts
+++ tests/default-panel/create-panel.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@grafana/plugin-e2e';
 
-test('should return clock panel', async ({ panelEditPage }) => {
+test('should return clock panel TEST', async ({ panelEditPage }) => {
   await panelEditPage.datasource.set('ClockTestData');
   await panelEditPage.setVisualization('Clock');
   await expect(panelEditPage.refreshPanel()).toBeOK();
